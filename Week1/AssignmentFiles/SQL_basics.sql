78f7ad8517ba747ee2009b2d910270f011a9ff12
Lindsay <lin.pageworks@gmail.com>
2026-06-09 19:15:56 -0500

    Completed SQL Murder Mystery step-by-step .ipynb file added - live class walkthrough

--- Week1/AssignmentFiles/SQL_basics.sql
+++ Week1/AssignmentFiles/SQL_basics.sql
@@ -10,22 +10,28 @@ USE coffeeshop_db;
 select name, price from products
 order by price desc;
 -- Q2) Show all customers who live in the city of 'Lihue'.
-
+select first_name, last_name from customers where city = 'Lihue';
 -- Q3) Return the first 5 orders by earliest order_datetime (order_id, order_datetime).
-
+select order_id, order_datetime from orders
+order by order_datetime
+limit 5;
 -- Q4) Find all products with the word 'Latte' in the name.
-
+select * from products
+where name like '%Latte%';
 -- Q5) Show distinct payment methods used in the dataset.
-
+select distinct payment_method from orders;
 -- Q6) For each store, list its name and city/state (one row per store).
-
+select name, city, state from stores;
 -- Q7) From orders, show order_id, status, and a computed column total_items
 --     that counts how many items are in each order.
 
 -- Q8) Show orders placed on '2025-09-04' (any time that day).
-
+select * from orders where order_datetime between "2025-09-04 00:00:00" and  "2025-09-04 23:59:59";
 -- Q9) Return the top 3 most expensive products (price, name).
-
+select price, name from products
+order by price desc
+limit 3;
 -- Q10) Show customer full names as a single column 'customer_name'
 --      in the format "Last, First".
+select 
 
